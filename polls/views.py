11da2a61219ce4f6f02d08992635bbf00748c12c
stevenzhang <steven@nowcast.co.jp>
2020-04-16 12:51:10 +0900

    initial commit

--- polls/views.py
+++ polls/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def index2(request):
+    return HttpResponse("这是一个测试.")
